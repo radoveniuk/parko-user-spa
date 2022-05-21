6306f7f5cbd9bde282a284e293eeb860069a5077
Bohdan Radoveniuk <bogdan.radoveniuk@globallogic.com>
2022-05-21 16:34:28 +0300

    add page queries hook

---
+++ src/hooks/usePageQueries.ts
@@ -0,0 +1,19 @@
+import { useLocation } from 'react-router-dom';
+
+export const getPageQueries = (url: string) => {
+  const params: { [paramKey: string]: string } = {};
+  url.substring(1).split('&').forEach((pair) => {
+    const [pairKey, pairValue] = pair.split('=').map(str => decodeURIComponent(str.replace(/\+/g, ' ')));
+    if (pairKey) {
+      params[pairKey] = pairValue;
+    }
+  });
+  return params;
+};
+
+const usePageQueries = () => {
+  const location = useLocation();
+  return getPageQueries(location.search);
+};
+
+export default usePageQueries;
