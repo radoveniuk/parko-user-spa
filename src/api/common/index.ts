64b1cfbbfbbbacbd03ed4caa0976b933a0ae625c
Bohdan Radoveniuk <bogdan.radoveniuk@gmail.com>
2022-06-23 12:46:42 +0200

    add proxy

--- src/api/common/index.ts
+++ src/api/common/index.ts
@@ -1,15 +1,13 @@
 import axios from 'axios';
 import { IFile } from 'interfaces/file.interface';
 
-const baseURL = 'http://localhost:3000';
-// const baseURL = 'http://31.131.20.202:3000';
+// "proxy": "http://localhost:3000",
 
 const api = axios.create({
-  baseURL,
   withCredentials: true,
 });
 
-export const uploadFiles = (formData: FormData): Promise<IFile[]> => fetch(`${baseURL}/files`, {
+export const uploadFiles = (formData: FormData): Promise<IFile[]> => fetch('/files', {
   method: 'POST',
   body: formData,
   redirect: 'follow',
