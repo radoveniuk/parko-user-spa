103c3e84c7f3bbacb249301faadeb9b6af202105
Bohdan Radoveniuk <bogdan.radoveniuk@gmail.com>
2022-08-17 16:52:05 +0200

    Update lang of Phone

--- src/components/shared/PhoneInput/PhoneInput.tsx
+++ src/components/shared/PhoneInput/PhoneInput.tsx
@@ -2,16 +2,21 @@ import React, { ForwardedRef, forwardRef } from 'react';
 import { MuiTelInputProps, isValidPhoneNumber } from 'mui-tel-input';
 
 import { StyledPhoneInput } from './styles';
+import { useTranslation } from 'react-i18next';
 
-const PhoneInput = (props: MuiTelInputProps, ref: ForwardedRef<HTMLInputElement>) => (
-  <StyledPhoneInput
-    ref={ref}
-    defaultCountry="SK"
-    preferredCountries={['UA', 'RU', 'SK']}
-    continents={['EU', 'AS']}
-    {...props}
-  />
-);
+const PhoneInput = (props: MuiTelInputProps, ref: ForwardedRef<HTMLInputElement>) => {
+  const { i18n } = useTranslation();
+
+  return (
+    <StyledPhoneInput
+      ref={ref}
+      defaultCountry="SK"
+      onlyCountries={['UA', 'RU', 'SK', 'GE', 'BY']}
+      langOfCountryName={i18n.language}
+      {...props}
+    />
+  );
+};
 
 export default forwardRef(PhoneInput);
 
