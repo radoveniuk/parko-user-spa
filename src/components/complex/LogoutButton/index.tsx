f1d5e9dbab2c575f6868b556d970ff034509be46
Bohdan Radoveniuk <bogdan.radoveniuk@gmail.com>
2022-08-21 14:36:30 +0200

    add confirm to logout

--- src/components/complex/LogoutButton/index.tsx
+++ src/components/complex/LogoutButton/index.tsx
@@ -1,15 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 
 import { useLogout } from 'contexts/AuthContext';
 import Button from 'components/shared/Button';
 import { ExitIcon } from 'components/icons';
+import DialogConfirm from 'components/shared/DialogConfirm';
 
 const LogoutButton = () => {
   const logout = useLogout();
   const navigate = useNavigate();
   const { t } = useTranslation();
+  const [open, setOpen] = useState(false);
 
   const logoutHandler = () => {
     logout();
@@ -17,7 +19,14 @@ const LogoutButton = () => {
   };
 
   return (
-    <Button onClick={logoutHandler} color="error" variant="outlined"><ExitIcon />{t('user.logout')}</Button>
+    <>
+      <Button onClick={() => void setOpen(true)} color="error" variant="outlined"><ExitIcon />{t('user.logout')}</Button>
+      <DialogConfirm
+        open={open}
+        onSubmit={logoutHandler}
+        onClose={() => void setOpen(false) }
+      />
+    </>
   );
 };
 
