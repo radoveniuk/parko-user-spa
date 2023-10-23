d08f88b707fc574107a8fa0a38f987af6f757bc5
bohdan radoveniuk <bogdan.radoveniuk@gmail.com>
2023-10-23 10:20:36 +0200

    Add storybook

---
+++ src/v2/uikit/Button/Button.stories.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { PlusIcon } from 'components/icons';
+
+import Button from '.';
+
+const meta: Meta<typeof Button> = {
+  component: Button,
+  argTypes: {
+    variant: {
+      name: 'variant',
+      description: 'Buttons view variant',
+      options: ['contained', 'outlined', 'text'],
+      control: { type: 'radio' },
+    },
+    children: {
+      name: 'children',
+      description: 'Buttons text',
+      control: { type: 'text' },
+    },
+    onClick: { action: 'clicked' },
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Button>;
+
+export const PrimaryButton: Story = {
+  args: {
+    variant: 'text',
+    children: <><PlusIcon /> Click</>,
+  },
+};
--- src/v2/uikit/Button/styles.ts
+++ src/v2/uikit/Button/styles.ts
@@ -2,7 +2,7 @@ import { Button as ButtonMui } from '@mui/material';
 import styled from 'styled-components';
 
 export const Button = styled(ButtonMui)`
-  box-shadow: none;
-  text-transform: unset;
+  box-shadow: none !important;
+  text-transform: unset !important;
   gap: 5px;
 `;
