dcf14639336c0ad803d8a8bd47bfe291425b18ba
bohdan radoveniuk <bogdan.radoveniuk@gmail.com>
2024-01-23 13:17:49 +0100

    fix

--- src/components/shared/FileUploadArea/FileUploadArea.tsx
+++ src/components/shared/FileUploadArea/FileUploadArea.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
-// @ts-ignore
-// @ts-nocheck
-import Dropzone, { IFileWithMeta, StatusValue } from 'react-dropzone-uploader';
+// import { IFileWithMeta, StatusValue } from 'react-dropzone-uploader';
 import { useTranslation } from 'react-i18next';
 
 import { FileUploadAreaWrapper } from './styles';
@@ -15,31 +13,32 @@ type Props = {
 }
 
 const FileUploadArea = ({ accept, fileNameValidator, onUpload }: Props) => {
+  // const Dropzone = require('dropzone');
   const { t } = useTranslation();
 
-  const handleChangeStatus = (file: IFileWithMeta, status: StatusValue) => {
-    if (status === 'done') {
-      if (fileNameValidator && !fileNameValidator(file.meta.name)) {
-        file.remove();
-      }
-    }
-  };
+  // const handleChangeStatus = (file: IFileWithMeta, status: StatusValue) => {
+  //   if (status === 'done') {
+  //     if (fileNameValidator && !fileNameValidator(file.meta.name)) {
+  //       file.remove();
+  //     }
+  //   }
+  // };
 
-  const handleSubmit = (files: IFileWithMeta[], allFiles: IFileWithMeta[]) => {
-    onUpload?.(files.map(f => f.file));
-    allFiles.forEach(f => f.remove());
-  };
+  // const handleSubmit = (files: IFileWithMeta[], allFiles: IFileWithMeta[]) => {
+  //   onUpload?.(files.map(f => f.file));
+  //   allFiles.forEach(f => f.remove());
+  // };
 
   return (
     <FileUploadAreaWrapper>
-      <Dropzone
+      {/* <Dropzone
         onChangeStatus={handleChangeStatus}
         onSubmit={handleSubmit}
         accept={accept}
         inputContent={t('file.upload')}
         submitButtonContent={t('file.submit')}
         inputWithFilesContent={t('file.add')}
-      />
+      /> */}
     </FileUploadAreaWrapper>
   );
 };
